3db14fdfc613a03b901bd50ceec593aa6707f7a4
zulibeth <zulitorres3@gmail.com>
2021-12-16 11:15:22 -0300

    otro cambio que no

--- src/index.js
+++ src/index.js
@@ -3,8 +3,4 @@ import cipher from './cipher.js';
 
 console.log(cipher);
 
- function cripto(){
-    var usercodes=document.getElementById("usercode").innerHtml="value"+ cipher;
-    alert(usercodes);
-    
-}
+
