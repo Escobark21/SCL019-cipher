ee3f3680313f479691a6a1bff44d92f95e7650c4
zulibeth <zulitorres3@gmail.com>
2021-12-16 11:14:12 -0300

    estos cambios no

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,25 @@
 const cipher = {
+  
   // ...
+  
+  
+    
+  
+  
 };
 
+
 export default cipher;
+public class ayuda{
+  private char letra;
+  letra=visualViewport;
+}
+public char getchat("A,B,C,D"){
+  return letra;
+}
+public int getcharaNum("0,1,2,3"){
+  return letra;
+}
+public void sumar (int num){
+  letra = (char) (letra+num);
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import cipher from './cipher.js';
 
+
 console.log(cipher);
+
+ function cripto(){
+    var usercodes=document.getElementById("usercode").innerHtml="value"+ cipher;
+    alert(usercodes);
+    
+}
