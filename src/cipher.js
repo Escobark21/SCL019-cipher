8ad00dcb9811037ad2969029c4269eed64418c5a
zulibeth <zulitorres3@gmail.com>
2022-01-04 07:30:01 -0300

    version de prueba antigua

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,46 @@
 const cipher = {
-  
+
+  encode: (texto, offset) => {
+    let resultado = "";
+    let letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    offset = (offset % 26 + 26) % 26;
+
+    if (texto) {
+      for (let i = 0; i < texto.length; i++) {
+        if (letras.indexOf(texto[i]) != -1) {
+          let posicion = ((letras.indexOf(texto[i]) + offset) % 26);
+          resultado += letras[posicion];
+
+        }
+        else
+          resultado += texto[i];
+      }
+    }
+    return resultado;
+  },
+
+  decode: (texto, offset) => {
+    let resultado = "";
+    let letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    offset = (offset % 26 - 26) % 26;
+
+    if (texto) {
+      for (let i = 0; i < texto.length; i++) {
+        if (letras.indexOf(texto[i]) != -1) {
+          let posicion = ((letras.indexOf(texto[i]) - offset) % 26);
+          resultado += letras[posicion];
+
+        }
+        else
+          resultado += texto[i];
+      }
+    }
+    return resultado;
+  }
+
+
 }
 export default cipher;
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 import cipher from './cipher.js';
 
-console.log("adios");
+window.addEventListener("load", inicio, true);
+
+function inicio(){
+
+document.getElementById("mensaje").addEventListener("keyup", function(){
+        this.value= this.value.toUpperCase();
+       }, true);}
+document.getElementById("cifrar").addEventListener("click", function(){
+        let texto = document.getElementById("mensaje").value;
+        let offset = document.getElementById("desplazamiento").value;
+        document.getElementById("mensaje2").value = cipher.encode(texto, offset);
+    }, true);
+
+document.getElementById("descifrar").addEventListener("click", function(){
+        let texto = document.getElementById("mensaje").value;
+        let offset = document.getElementById("desplazamiento").value;
+        document.getElementById("mensaje2").value = cipher.decode(texto, offset);
+    }, true);
+
+
+
 
